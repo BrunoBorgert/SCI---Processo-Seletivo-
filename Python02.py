567766168161b70cbec40ad4d394fc6fb8bdd68b
BrunoBorgert <113982921+BrunoBorgert@users.noreply.github.com>
2023-03-19 21:44:03 -0300

    adicionando segunda resposta

---
+++ Python02.py
@@ -0,0 +1,14 @@
+# BRUNO GUILHERME BORGERT - PYTHON
+
+numeros = []
+print("Sistema Python 02")
+for i in range(5):
+    numero = float(input("Digite um número: "))
+    numeros.append(numero)
+
+maior = max(numeros)
+menor = min(numeros)
+
+print("O número maior é:", maior)
+print("O número menor é:", menor)
+print("Muito Obrigado por utilizar nossos serviços")
