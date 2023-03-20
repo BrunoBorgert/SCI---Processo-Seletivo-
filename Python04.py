159ab15711b2fb953a66f314910887683e36a75f
BrunoBorgert <113982921+BrunoBorgert@users.noreply.github.com>
2023-03-19 21:51:21 -0300

    adicionando quarta resposta

---
+++ Python04.py
@@ -0,0 +1,10 @@
+# BRUNO GUILHERME BORGERT - PYTHON
+
+print("Sistema Python 03")
+vetor = [0] * 5
+
+for i in range(5):
+    vetor[i] = float(input("Digite um número: "))
+
+print("O valor na posição três é:", vetor[2])
+print("Obrigado por utilizar nosso serviços")
